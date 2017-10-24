722f30f556d75e1d3632a91b76e5b3fb3767c608
Daniel Eriksen <mumien@hotmail.com>
2017-10-24 16:18:22 +0200

    improved movement, add house prefab

---
+++ Peasant/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private Animator anim;
+    private Rigidbody2D myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       moveplayer();
+    }
+
+
+    void moveplayer()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        if (inputX == 1 || inputX == -1)
+        {
+            inputY = 0f;
+            myRigidbody.velocity = new Vector2(inputX * moveSpeed, 0);
+        }
+        else if (inputY == 1 || inputY == -1)
+        {
+            inputX = 0f;
+            myRigidbody.velocity = new Vector2(0, inputY * moveSpeed);
+        }
+
+        if (inputX == 0 && inputY == 0)
+        {
+            myRigidbody.velocity = new Vector2(0f, 0f);
+        }
+
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+    }
+}
