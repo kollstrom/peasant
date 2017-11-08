9d43f46473f6ce830cf1ef726894b49866a5ceb4
kollstrom <e.kollstrom@gmail.com>
2017-11-08 21:57:38 +0100

    Add UI space button on talkable collider

---
+++ Assets/Scripts/DialogHolder.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogHolder : MonoBehaviour {
+
+    public string dialogue;
+
+    public GameObject imageGameObject;
+
+	// Use this for initialization
+	void Start () {
+        imageGameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            // Show "Space" icon
+            imageGameObject.SetActive(true);
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                // Show dialogue box
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // Hide "Space" icon
+        if(collision.gameObject.name == "Player")
+        {
+            imageGameObject.SetActive(false);
+        }
+    }
+}
