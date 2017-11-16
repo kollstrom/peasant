b4b850cc13e8dc9aa1fe32cf30af8643391411cc
Daniel Eriksen <mumien@hotmail.com>
2017-11-16 11:53:14 +0100

    fix guard scare stop error

--- Assets/Scripts/CatchPlayer.cs
+++ Assets/Scripts/CatchPlayer.cs
@@ -6,7 +6,6 @@ public class CatchPlayer : MonoBehaviour {
 
     private GuardContainer guardCont;
     private BoxCollider2D box2d;
-    private GameObject parent;
     private bool isScarableGuard;
     public LayerMask layermask;
 
@@ -14,36 +13,11 @@ public class CatchPlayer : MonoBehaviour {
     void Start () {
         guardCont = transform.parent.parent.gameObject.GetComponent<GuardContainer>();
         box2d = transform.gameObject.GetComponent<BoxCollider2D>();
-        parent = transform.parent.gameObject;
-        if(parent.tag == "MovingGuard" || parent.tag == "StandingGuard")
-        {
-            isScarableGuard = true;
-        }
-        else
-        {
-            isScarableGuard = false;
-        }
 }
 	
 	// Update is called once per frame
 	void Update () {
-        if (isScarableGuard)
-        {
-            parent.transform.GetChild(1).GetComponent<PolygonCollider2D>().enabled = false;
-        }
-
-        parent.GetComponent<BoxCollider2D>().enabled = false;
-        box2d.enabled = false;
-
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, 30, layermask);
-
-        box2d.enabled = true;
-        parent.GetComponent<BoxCollider2D>().enabled = true;
-
-        if (isScarableGuard)
-        {
-            parent.transform.GetChild(1).GetComponent<PolygonCollider2D>().enabled = true;
-        }
 
         if (hit.collider != null)
         {
