7bc421f4e068c75a337384ead6cf0b54e75abf47
Daniel Eriksen <mumien@hotmail.com>
2017-11-16 11:42:56 +0100

    add guard prefab, guards dont see through walls

--- Assets/Scripts/CatchPlayer.cs
+++ Assets/Scripts/CatchPlayer.cs
@@ -5,16 +5,53 @@ using UnityEngine;
 public class CatchPlayer : MonoBehaviour {
 
     private GuardContainer guardCont;
+    private BoxCollider2D box2d;
+    private GameObject parent;
+    private bool isScarableGuard;
+    public LayerMask layermask;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         guardCont = transform.parent.parent.gameObject.GetComponent<GuardContainer>();
-    }
+        box2d = transform.gameObject.GetComponent<BoxCollider2D>();
+        parent = transform.parent.gameObject;
+        if(parent.tag == "MovingGuard" || parent.tag == "StandingGuard")
+        {
+            isScarableGuard = true;
+        }
+        else
+        {
+            isScarableGuard = false;
+        }
+}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (isScarableGuard)
+        {
+            parent.transform.GetChild(1).GetComponent<PolygonCollider2D>().enabled = false;
+        }
+
+        parent.GetComponent<BoxCollider2D>().enabled = false;
+        box2d.enabled = false;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, 30, layermask);
+
+        box2d.enabled = true;
+        parent.GetComponent<BoxCollider2D>().enabled = true;
+
+        if (isScarableGuard)
+        {
+            parent.transform.GetChild(1).GetComponent<PolygonCollider2D>().enabled = true;
+        }
+
+        if (hit.collider != null)
+        {
+            box2d.offset = new Vector2(box2d.offset.x, -hit.distance/2);
+            box2d.size = new Vector2(box2d.size.x, hit.distance);
+            //print(hit.collider.name);
+        }
+    }
 
 
     public void turn(float direction)
