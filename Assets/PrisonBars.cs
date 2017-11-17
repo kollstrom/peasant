1354774fd3ca373559aaabd74f5f725d131b2cab
Daniel Eriksen <mumien@hotmail.com>
2017-11-17 09:40:10 +0100

    add last room in dungeon

---
+++ Assets/PrisonBars.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrisonBars : MonoBehaviour {
+
+    public GameObject spaceButtonImage;
+    private bool playerInTrigger;
+    public DialogueHolder dialog;
+
+    // Use this for initialization
+    void Start () {
+        spaceButtonImage.SetActive(false);
+        playerInTrigger = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Space) && playerInTrigger)
+        {
+            gameObject.SetActive(false);
+            spaceButtonImage.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            playerInTrigger = true;
+            spaceButtonImage.SetActive(true);
+        }
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInTrigger = false;
+            spaceButtonImage.SetActive(false);
+        }
+    }
+}
