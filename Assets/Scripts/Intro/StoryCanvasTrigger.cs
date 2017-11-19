c50fb6f597a64d071e07dba5f2a76882e9e7927d
kollstrom <e.kollstrom@gmail.com>
2017-11-19 11:02:24 +0100

    Add story dialogue through the woods

---
+++ Assets/Scripts/Intro/StoryCanvasTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryCanvasTrigger : MonoBehaviour {
+
+	private BoxCollider2D trigger;
+    private Animator animator;
+    private GameObject player;
+    private PolygonCollider2D playerCollider;
+    private bool isTouching = false;
+
+	void Start () {
+        trigger = GetComponentInChildren<BoxCollider2D>();
+        animator = GetComponentInChildren<Animator>();
+        playerCollider = GameObject.Find("Player").GetComponent<PolygonCollider2D>();
+	}
+	
+	void Update () {
+        bool wasTouching = isTouching;
+        if (trigger.IsTouching(playerCollider))
+        {
+            isTouching = true;
+        }
+        else {
+            isTouching = false;
+        }
+
+        if (wasTouching != isTouching)
+        {
+            animator.SetBool("show", isTouching);
+        }
+	}
+}
