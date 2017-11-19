4f774dece156b35434d0dd959a60b66d04b2e780
kollstrom <e.kollstrom@gmail.com>
2017-11-19 01:19:06 +0100

    Add intro

---
+++ Assets/Scripts/Intro/IntroDialogueHolder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroDialogueHolder : MonoBehaviour {
+
+    public Dialogue dialogue;
+
+    private DialogueManager dialogueManager;
+    public float timeBetweenDialogue;
+    private float timeLeft;
+    private bool hasStarted = false;
+
+	void Start () {
+        dialogueManager = FindObjectOfType<DialogueManager>();
+
+	}
+	
+	void Update () {
+        if (!hasStarted)
+        {
+            dialogueManager.StartDialogue(dialogue);
+            timeLeft = timeBetweenDialogue;
+            hasStarted = true;
+        }
+        else if (timeLeft <= 0)
+        {
+            if (dialogueManager.HasMoreSentences())
+            {
+                dialogueManager.DisplayNextSentence();
+            }
+            else 
+            {
+                gameObject.SetActive(false);
+                SceneManager.LoadSceneAsync("World");
+            }
+        }
+        timeLeft -= Time.deltaTime;
+	}
+}
